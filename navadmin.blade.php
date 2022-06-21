a7a826f7ed9f05a811cc22af9337a1e5538bae6a
quan16041998 <107875007+quan16041998@users.noreply.github.com>
2022-06-21 14:58:41 +0700

    Add files via upload

---
+++ navadmin.blade.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand navbar-light bg-light">
+    <div class="container">
+        <ul class="navbar-nav mr-auto">
+{{--            <li class="nav-item">--}}
+{{--                <a class="nav-link" href="{{route('manager.classindex')}}">Admin</a>--}}
+{{--            </li>--}}
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('admin.collectionindex')}}">Collection</a>
+            </li>
+{{--            <li class="nav-item">--}}
+{{--                <a class="nav-link" href="{{route('manager.studentindex')}}">Stylist</a>--}}
+{{--            </li>--}}
+{{--            <li class="nav-item">--}}
+{{--                <a class="nav-link" href="{{route('manager.studentindex')}}">Product</a>--}}
+{{--            </li>--}}
+        </ul>
+    </div>
+</nav>
