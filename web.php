b51417acd6fa762c384be0926a6bd1e04632e670
tuanbui03 <101034908+tuanbui03@users.noreply.github.com>
2022-06-21 22:22:11 +0700

    Update web.php

--- web.php
+++ web.php
@@ -13,22 +13,81 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::group(['prefix'=>'admin'], function (){
-    Route::get('',[
-        'uses'=>'AdminControllerWithRepos@adminindex',
-        'as' => 'admin.adminindex'
+Route::group(['prefix' => 'adminrepos'], function () {
+    Route::get('', [
+        'uses' => 'AdminControllerWithRepos@index',
+        'as' => 'admin.index'
     ]);
-    Route::get('collection',[
-        'uses' => 'AdminControllerWithRepos@collectionindex',
-        'as' => 'admin.collectionindex'
+
+    Route::get('create', [
+        'uses' => 'AdminControllerWithRepos@create_product',
+        'as' => 'admin.create_products'
     ]);
+
+    Route::get('create', [
+        'uses' => 'AdminControllerWithRepos@create_collection',
+        'as' => 'admin.create_collection'
+    ]);
+
+    Route::get('create', [
+        'uses' => 'AdminControllerWithRepos@create_customers',
+        'as' => 'admin.create_customers'
+    ]);
+
+    Route::get('create', [
+        'uses' => 'AdminControllerWithRepos@create_stylist',
+        'as' => 'admin.create_stylist'
+    ]);
+
+    Route::get('create', [
+        'uses' => 'AdminControllerWithRepos@store',
+        'as' => 'admin.store'
+    ]);
+
+    Route::get('update/{ID}', [
+        'uses' => 'AdminControllerWithRepos@update_products',
+        'as' => 'admin.update_products'
+    ]);
+
+    Route::get('update/{ID}', [
+        'uses' => 'AdminControllerWithRepos@update_collection',
+        'as' => 'admin.update_collection'
+    ]);
+
+    Route::get('update/{ID}', [
+        'uses' => 'AdminControllerWithRepos@update_customers',
+        'as' => 'admin.update_customers'
+    ]);
+
+    Route::get('update/{ID}', [
+        'uses' => 'AdminControllerWithRepos@update_stylist',
+        'as' => 'admin.update_stylist'
+    ]);
+
+    Route::get('show/{ID}',[
+        'uses' => 'AdminControllerWithRepos@show_products',
+        'as' => 'admin.show_products'
+    ]);
+
+    Route::get('show/{ID}',[
+        'uses' => 'AdminControllerWithRepos@show_collection',
+        'as' => 'admin.show_collection'
+    ]);
+
+    Route::get('show/{ID}',[
+        'uses' => 'AdminControllerWithRepos@show_customers',
+        'as' => 'admin.show_customers'
+    ]);
+
+    Route::get('show/{ID}',[
+        'uses' => 'AdminControllerWithRepos@show_stylist',
+        'as' => 'admin.show_stylist'
+    ]);
+
+    Route::get('show/{ID}',[
+        'uses' => 'AdminControllerWithRepos@show_admin',
+        'as' => 'admin.show_admin'
+    ]);
+
 });
-
-
-
-
-
 
