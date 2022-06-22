bee4a672d8e43e965211f9f3bed71f2756b20ca2
tuanbui03 <101034908+tuanbui03@users.noreply.github.com>
2022-06-22 10:41:11 +0700

    Add files via upload

---
+++ AdminControllerWithRepos.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repository\AdminRepos;
+use Dotenv\Validator;
+use Illuminate\Http\Request;
+
+class AdminControllerWithRepos extends Controller
+{
+    public function index()
+    {
+        $admin = AdminRepos::getAllAdmin();
+        return view('admin.index',
+            [
+                'admin' => $admin
+            ]);
+    }
+
+    public function show($username)
+    {
+        $admin = AdminRepos::getAdminByUsername($username);
+        return view('admin.show', [
+            'admin' => $admin[0]
+        ]);
+    }
+
+    public function edit($username)
+    {
+        $username = AdminRepos::getClassByUsername($username); //this is always an array
+
+
+        return view(
+            'admin.update',
+            ["username" => $username[0]]);
+    }
+
+    public function update(Request $request, $username)
+    {
+        if ($username != $request->input('username')) {
+            return redirect()->action('AdminControllerWithRepos@index');
+        }
+        $this->formValidate($request)->validate();
+
+        $admin = (object)[
+            'username' => $request->input('username'),
+            'password' => $request->input('password'),
+            'name' => $request->input('name'),
+            'dob' => $request->input('dob'),
+            'contact' => $request->input('contact'),
+            'email' => $request->input('email'),
+        ];
+
+        AdminRepos::update($admin);
+
+        return redirect()->action('AdminControllerWithRepos@index')
+            ->with('msg', 'Update Successfully');
+    }
+
+    private function formValidate(Request $request)
+    {
+        return Validator::make(
+            $request->all(),
+            [
+                'username' => ['required', 'min:6'],
+                'password' => ['required', 'min:6'],
+                'name' => ['required', 'min:6'],
+                'dob' => ['required'],
+                'contact' => ['required'],
+                'email' => ['email']
+            ],
+            [
+                'username.required' => 'Username can not be empty',
+                'password.required' => 'Password can not be empty',
+                'name.required' => 'Name can not be empty',
+                'dob.required' => 'Date of birth can not be empty',
+                'contact.required' => 'Contact can not be empty',
+                'email.required' => 'Email can not be empty',
+                'username.min' => 'Username must have at least 6 characters',
+                'password.min' => 'Password must have at least 6 characters',
+                'name.min' => 'name must have at least 6 characters'
+            ]
+        );
+
+}
+
+}
