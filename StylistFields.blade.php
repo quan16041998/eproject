601bc347c2583892070ebb5e81333aab0d478f0c
KhacDuongg <107907295+KhacDuongg@users.noreply.github.com>
2022-06-23 00:34:09 +0700

    Update StylistFields.blade.php

--- StylistFields.blade.php
+++ StylistFields.blade.php
@@ -1,32 +1,32 @@
 <input type="hidden" name="SID" value="{{old('SID')?? $stylist->SID}}">
 
 <div class="form-group">
-  <label for="title" class="font-weight-bold">Stylist Name</label>
+  <label for="name" class="font-weight-bold">Stylist Name</label>
   <input type="text" class="form-control" id="name" name="name" value="{{old('name')?? $stylist->name}}">
 </div>
 
 <div class="form-group">
-  <label for="author" class="font-weight-bold">dob</label>
+  <label for="dob" class="font-weight-bold">dob</label>
   <input type="date" class="form-control" id="dob" name="dob" value="{{old('dob')?? $stylist->dob}}">
 </div>
 
 <div class="form-group">
-  <label for="pages" class="font-weight-bold">contact</label>
+  <label for="contact" class="font-weight-bold">contact</label>
   <input type="text" class="form-control" id="contact" name="contact" value="{{old('contact')?? $stylist->contact}}" >
 </div>
 
 <div class="form-group">
-  <label for="pages" class="font-weight-bold">email</label>
+  <label for="email" class="font-weight-bold">email</label>
   <input type="email" class="form-control" id="email" name="email" value="{{old('email')?? $stylist->email}}" >
 </div>
 
 <div class="form-group">
-  <label for="pages" class="font-weight-bold">history</label>
+  <label for="history" class="font-weight-bold">history</label>
   <input type="text" class="form-control" id="history" name="history" value="{{old('history')?? $stylist->history}}" >
 </div>
 
 <div class="form-group">
-  <label for="pages" class="font-weight-bold">urlimg</label>
+  <label for="urlimg" class="font-weight-bold">urlimg</label>
   <input type="text" class="form-control" id="urlimg" name="urlimg" value="{{old('urlimg')?? $stylist->urlimg}}" >
 </div>
 
