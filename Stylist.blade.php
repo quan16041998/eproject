22da60c640256e7f2b321bdc046a5822b60bccc6
KhacDuongg <107907295+KhacDuongg@users.noreply.github.com>
2022-06-22 13:52:18 +0700

    Update Stylist.blade.php

--- Stylist.blade.php
+++ Stylist.blade.php
@@ -14,17 +14,9 @@
 </head>
 
 <body>
-<header>
-  @include('partials.teacherNav')
-  <br><br>
-</header>
-
 <main role="main">
   @yield('main')
 </main>
-
-
-
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
@@ -32,7 +24,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"
         integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF"
         crossorigin="anonymous"></script>
-
 
 </body>
 
