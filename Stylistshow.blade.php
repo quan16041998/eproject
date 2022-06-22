7ea7ef987fc4719d8d61a639988fddd5560b89c3
KhacDuongg <107907295+KhacDuongg@users.noreply.github.com>
2022-06-22 18:35:32 +0700

    Update Stylistshow.blade.php

--- Stylistshow.blade.php
+++ Stylistshow.blade.php
@@ -6,6 +6,6 @@
   <div class="container">
     <h1 class="display-4">Stylist Details</h1>
     @include('stylist.StylistDetails')
-    <a type="button" href="{{route('stylist.index')}}" class="btn btn-info">&lt;&lt;&nbsp;Index</a>
+    <a type="button" href="{{route('stylist.index')}}" class="btn btn-info">&lt;&lt;&nbsp;Back</a>
   </div>
 @endsection
