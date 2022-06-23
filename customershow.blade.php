b7e890712f39472816ba2ddaf360d39d1f2ed13f
KhacDuongg <107907295+KhacDuongg@users.noreply.github.com>
2022-06-23 12:55:11 +0700

    Update customershow.blade.php

--- customershow.blade.php
+++ customershow.blade.php
@@ -4,6 +4,6 @@
   <div class="container">
     <h1 class="display-4">Customer Details</h1>
     @include('customer.customerDetails')
-    <a type="button" href="{{route('Admin.index_customer')}}" class="btn btn-info">&lt;&lt;&nbsp;Back</a>
+    <a type="button" href="{{route('Customer.index_customer')}}" class="btn btn-info">&lt;&lt;&nbsp;Back</a>
   </div>
 @endsection
