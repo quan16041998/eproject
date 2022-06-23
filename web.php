75ee03df783fdc72d66cb9a00c4de76ab24d9a37
tuanbui03 <101034908+tuanbui03@users.noreply.github.com>
2022-06-23 11:23:13 +0700

    Update web.php

--- web.php
+++ web.php
@@ -19,101 +19,66 @@ Route::group(['prefix' => 'adminrepos'], function () {
         'as' => 'admin.index'
     ]);
 
-    Route::get('create', [
-        'uses' => 'AdminControllerWithRepos@create_product',
-        'as' => 'admin.create_products'
+    Route::get('show/{username}', [
+        'uses' => 'AdminControllerWithRepos@show',
+        'as' => 'admin.show'
+    ]);
+
+    Route::get('update/{username}', [
+        'uses' => 'AdminControllerWithRepos@edit',
+        'as' => 'admin.edit'
+    ]);
+
+    Route::post('update/{username}', [
+        'uses' => 'AdminControllerWithRepos@update',
+        'as' => 'admin.update'
+    ]);
+
+});
+
+
+Route::group(['prefix' => 'productsrepos'], function () {
+
+    Route::get('', [
+        'uses' => 'ProductsControllerWithRepos@index',
+        'as' => 'product.index'
+    ]);
+
+    Route::get('show/{CID}', [
+        'uses' => 'ProductsControllerWithRepos@show',
+        'as' => 'product.show'
     ]);
 
     Route::get('create', [
-        'uses' => 'AdminControllerWithRepos@create_collection',
-        'as' => 'admin.create_collection'
+        'uses' => 'ProductsControllerWithRepos@create',
+        'as' => 'product.create'
     ]);
 
-    Route::get('create', [
-        'uses' => 'AdminControllerWithRepos@create_customers',
-        'as' => 'admin.create_customers'
+    Route::post('create', [
+        'uses' => 'ProductsControllerWithRepos@store',
+        'as' => 'product.store'
     ]);
 
-    Route::get('create', [
-        'uses' => 'AdminControllerWithRepos@create_stylist',
-        'as' => 'admin.create_stylist'
+    Route::get('update/{CID}', [
+        'uses' => 'ProductsControllerWithRepos@edit',
+        'as' => 'product.edit'
     ]);
 
-    Route::get('create', [
-        'uses' => 'AdminControllerWithRepos@store',
-        'as' => 'admin.store'
+    Route::post('update/{CID}', [
+        'uses' => 'ProductsControllerWithRepos@update',
+        'as' => 'product.update'
     ]);
 
-    Route::get('update/{ID}', [
-        'uses' => 'AdminControllerWithRepos@update_products',
-        'as' => 'admin.update_products'
+    Route::get('delete/{CID}', [
+        'uses' => 'ProductsControllerWithRepos@confirm',
+        'as' => 'product.confirm'
     ]);
 
-    Route::get('update/{ID}', [
-        'uses' => 'AdminControllerWithRepos@update_collection',
-        'as' => 'admin.update_collection'
+    Route::post('delete/{CID}', [
+        'uses' => 'ProductsControllerWithRepos@destroy',
+        'as' => 'product.destroy'
     ]);
-
-    Route::get('update/{ID}', [
-        'uses' => 'AdminControllerWithRepos@update_customers',
-        'as' => 'admin.update_customers'
-    ]);
-
-    Route::get('update/{ID}', [
-        'uses' => 'AdminControllerWithRepos@update_stylist',
-        'as' => 'admin.update_stylist'
-    ]);
-
-    Route::get('show/{ID}',[
-        'uses' => 'AdminControllerWithRepos@show_products',
-        'as' => 'admin.show_products'
-    ]);
-
-    Route::get('show/{ID}',[
-        'uses' => 'AdminControllerWithRepos@show_collection',
-        'as' => 'admin.show_collection'
-    ]);
-
-    Route::get('show/{ID}',[
-        'uses' => 'AdminControllerWithRepos@show_customers',
-        'as' => 'admin.show_customers'
-    ]);
-
-    Route::get('show/{ID}',[
-        'uses' => 'AdminControllerWithRepos@show_stylist',
-        'as' => 'admin.show_stylist'
-    ]);
-
-    Route::get('show/{ID}',[
-        'uses' => 'AdminControllerWithRepos@show_admin',
-        'as' => 'admin.show_admin'
-    ]);
-
-    Route::get('delete/{ID}', [
-        'uses' => 'AdminControllerWithRepos@confirm_products',
-        'as' => 'admin.confirm_products'
-    ]);
-
-    Route::get('delete/{ID}', [
-        'uses' => 'AdminControllerWithRepos@confirm_collection',
-        'as' => 'admin.confirm_collection'
-    ]);
-
-    Route::get('delete/{ID}', [
-        'uses' => 'AdminControllerWithRepos@confirm_customers',
-        'as' => 'admin.confirm_customers'
-    ]);
-
-    Route::get('delete/{ID}', [
-        'uses' => 'AdminControllerWithRepos@confirm_stylist',
-        'as' => 'admin.confirm_stylist'
-    ]);
-
-    Route::post('delete/{ID}',[
-        'uses' => 'AdminControllerWithRepos@destroy',
-        'as' => 'admin.destroy'
-    ]);
-    
+});
     
 Route::group(['prefix' => 'stylistrepos'], function () {
     Route::get('', [
